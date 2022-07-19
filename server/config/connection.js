49229c1bf1049831a2592da13bb66f292ca088ad
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-07-18 23:11:56 -0400

    set up the project

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/mern_shopping', 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
