12850e878072ba5642f6ab458fe7fff7c12fdd8c
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-08-02 17:27:23 -0400

    add actions and redcuers
    add test to simulate the reducers

---
+++ client/src/utils/actions.js
@@ -0,0 +1,6 @@
+//Actions: The types of events that can be emitted to update state.
+//State can only be updated if its a predefined action.
+
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
---
+++ client/src/utils/reducers.js
@@ -0,0 +1,40 @@
+//The actual functionality that carries out the emitted action to update state.
+import {useReducer} from 'react';
+
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from "./actions";
+
+export const reducer = (state,action) => {
+    switch (action.type){
+        //if action type is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+        case UPDATE_PRODUCTS:
+            return{
+                ...state,
+                products: [...action.products],
+            };
+        
+        case UPDATE_CATEGORIES:
+            return {
+                ...state, 
+                categories: [...action.categories]
+            };
+
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            }
+
+        //if it's none of these actions, do not update state at all and keep things the same!
+        default: 
+            return state;    
+    }
+};
+
+//this function will help initialize the global state object and provide with the functionality for updating that state by running custom reducer() function.
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+}
